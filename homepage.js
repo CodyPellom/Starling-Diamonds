2c4cd60f432ab013965475c03b6bf1d4abf713aa
Cody Pellom <cpellom@appliedsystems.com>
2019-11-03 09:33:01 -0500

    'frontPageStylingProgress'

--- homepage.js
+++ homepage.js
@@ -1,4 +1,9 @@
-$(document).ready(function(){
-    $(".dropdown-trigger").dropdown();
 
-})
+var navLinks = document.getElementsByClassName("nav-links");
+window.onload = function(){
+for(var i = 0, j = this.navLinks.length; i < j; i++){
+    this.navLinks[i].style.transition = "color 3s";
+    this.navLinks[i].style.color = "black";
+}
+    
+}
